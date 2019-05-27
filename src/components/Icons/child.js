e8ccd7dbe533dc8caa309c8b4e73c1ede385c0e0
topsoftwareguru <janepress940214@outlook.com>
2019-05-27 11:36:46 +0800

    child icons

--- src/components/Icons/child.js
+++ src/components/Icons/child.js
@@ -12,7 +12,7 @@ const ChildIcon = ({
       fill="none"
       stroke="#fff"
     >
-      <g id="Layer_2"> <g id="Outline_Icons"> <path className="st0" d="M2.5,12.5h6c1.1,0,2,0.9,2,2v6c0,1.1-0.9,2-2,2h-6c-1.1,0-2-0.9-2-2v-6C0.5,13.4,1.4,12.5,2.5,12.5z"></path> <path className="st0" d="M15.5,12.5h6c1.1,0,2,0.9,2,2v6c0,1.1-0.9,2-2,2h-6c-1.1,0-2-0.9-2-2v-6C13.5,13.4,14.4,12.5,15.5,12.5z"></path> <path className="st0" d="M8.5,0.5h6c1.1,0,2,0.9,2,2v6c0,1.1-0.9,2-2,2h-6c-1.1,0-2-0.9-2-2v-6C6.5,1.4,7.4,0.5,8.5,0.5z"></path> <path className="st1" d="M3.5,14.5H6c0.8,0,1.5,0.7,1.5,1.5c0,0.8-0.7,1.5-1.5,1.5H3.5V14.5z"></path> <path className="st1" d="M3.5,17.5H6c0.8,0,1.5,0.7,1.5,1.5S6.8,20.5,6,20.5H3.5V17.5z"></path> <polyline className="st0" points="9,8.5 11.6,2.5 14,8.5 		"></polyline> <line className="st0" x1="9.9" y1="6.5" x2="13.2" y2="6.5"></line> <path className="st0" d="M21.1,19.6c-1.2,1.2-3.1,1.2-4.2,0c-1.2-1.2-1.2-3.1,0-4.2c1.1-1.1,3-1.2,4.2-0.1"></path> </g> </g>
+      <g fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10"><path d="M11.51.48h-.018C9.45 3.016 5.832 4.025 1.5 1v14.812s1.45 5.27 10 7.668c8.55-2.396 10-7.668 10-7.668V1c-4.307 3.008-7.945 2.023-9.99-.52z"></path><path d="M17.535 8.47L11.5 17.2l-4.578-4.58"></path></g>
     </svg>
   )
 export default ChildIcon;
