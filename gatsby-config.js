f4325c1e164781fdb60fb63d17ff7ab40e56f92b
topsoftwareguru <janepress940214@outlook.com>
2019-06-14 21:24:49 +0800

    404 page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,5 @@
 module.exports = {
   siteMetadata: {
-    title: `Blogs from WordPress`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     menuLinks: [
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,39 @@
 import React from "react"
+import { graphql } from 'gatsby';
 
 import Layout from "../components/layout"
+import '../components/blogs/latestblog';
 import SEO from "../components/seo"
+import LatestBlog from "../components/blogs/latestblog";
 
-const NotFoundPage = () => {
+const NotFoundPage = (props) => {
   typeof window !== `undefined` && (window.document.title = "404 | UCare");
+  console.log(props.data.file.publicURL);
   return (
-  <Layout>
-    <SEO title="404: Not found" />
-    
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+      <SEO title="404 | UCare" />
+      <div className="container-fluid p-0 page-404" style={ {
+        background: `linear-gradient( rgba(50,58,70,.5), rgba(50,58,70,.5) ), url(${props.data.file.publicURL})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: `cover`,
+        backgroundPosition: `50%`,
+      }}>
+        <h1 className="text-white text-center">FOUR, OH FOUR.</h1>
+      </div>
+      <div className="not-exist">
+        <p className="text-center">Sorry, but the page you were trying to view does not exist.</p>
+      </div>
+      <LatestBlog />
+    </Layout>
   )
 }
 
-export default NotFoundPage
+export default NotFoundPage;
+
+export const get404ImageQuery = graphql`
+  query get404ImageQuery{
+    file(relativePath: { eq: "404.jpg" }) {
+      publicURL
+    }
+  }
+`
