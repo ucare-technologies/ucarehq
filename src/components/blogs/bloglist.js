b49610011a6cda4c22cf962593fc7b7c428ce2fd
Des Browne <des@ucarehq.com>
2019-06-27 01:32:19 +1000

    prettier format

--- src/components/blogs/bloglist.js
+++ src/components/blogs/bloglist.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'gatsby';
 /**
@@ -7,43 +7,47 @@ import { Link } from 'gatsby';
  * Using fontawesome
  */
 class BlogList extends Component {
-  render() { 
-    const { title, date, url, excerpt, imageURL } = this.props;
-    return ( 
-      <div className="container bloglist">
-        <div className="row">
-          <div className="col-md-9">
-            <div className="entry-image">
-              { imageURL && 
-                <Link to={ url } >
-                  <img src={imageURL} alt="entry" />
-                </Link>
-              }
-            </div>
-            <div className="entry-content">
-              <article>
-                <h2>
-                  <Link to={ url } className="blog-title">{ title }</Link>
-                </h2>
-                <span>
-                  <FontAwesomeIcon icon={ faCalendarAlt } />&nbsp;&nbsp;
-                  { date }
-                </span>
-                <div className="entry-summary">
-                  <p>{ excerpt }</p>
-                </div>
-              </article>
-              <div>
-                <a href={url} className="readmore">
-                    Read More&nbsp;&nbsp;<FontAwesomeIcon icon={ faChevronRight } />
-                </a>                
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-     );
-  }
+	render() {
+		const { title, date, url, excerpt, imageURL } = this.props;
+		return (
+			<div className='container bloglist'>
+				<div className='row'>
+					<div className='col-md-9'>
+						<div className='entry-image'>
+							{imageURL && (
+								<Link to={url}>
+									<img src={imageURL} alt='entry' />
+								</Link>
+							)}
+						</div>
+						<div className='entry-content'>
+							<article>
+								<h2>
+									<Link to={url} className='blog-title'>
+										{title}
+									</Link>
+								</h2>
+								<span>
+									<FontAwesomeIcon icon={faCalendarAlt} />
+									&nbsp;&nbsp;
+									{date}
+								</span>
+								<div className='entry-summary'>
+									<p>{excerpt}</p>
+								</div>
+							</article>
+							<div>
+								<a href={url} className='readmore'>
+									Read More&nbsp;&nbsp;
+									<FontAwesomeIcon icon={faChevronRight} />
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
- 
+
 export default BlogList;
--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -14,112 +14,105 @@ import './_bloglist.scss';
  * Gatsby pagination
  */
 class BlogList extends Component {
-  render() { 
-    const { data: { blog } } = this.props;
-    const posts = this.props.data.allMarkdownRemark.edges
-    const { totalCount } = this.props.data.allMarkdownRemark;
-    const numPages = Math.ceil(totalCount / 5);
-    const { currentPage } = this.props.pageContext;
-    const isFirst = currentPage === 1;
-    const isLast = currentPage === numPages;
-    typeof window !== `undefined`
-      && (window.document.title = `${currentPage === 1 ? 'Blog | Ucare': `Blog | Page ${currentPage} | UCare`}`)
-    const prevPage = currentPage - 1 === 1 ? "/blog" : `/blog/page/${(currentPage - 1).toString()}`
-    const nextPage = `/blog/page/${(currentPage + 1).toString()}`
-    return ( 
-      <Layout>
-        <div className="container-fluid p-0 blog-header text-center" style={ {
-          background: `linear-gradient( rgba(50,58,70,.5), rgba(50,58,70,.5) ), url(${blog.publicURL})`,
-          backgroundColor: '#323a46',
-          backgroundPosition: '50%',
-          backgroundSize: 'cover'
-        } }>
-          <div className="centered">
-            <h1>Blog</h1>
-            <h6>Latest News & Updates from the UCare Team</h6>
-          </div> 
-        </div>
-        {posts.map(({ node }) => {
-          const { title, date, featured_image } = node.frontmatter;
-          let publicURL = null;
-          if (featured_image) publicURL = featured_image.publicURL;
-          const { slug } = node.fields;
-          const { excerpt } = node;
-          return <BlogListTemp
-                  title={ title }
-                  date={ date }
-                  url={ slug }
-                  excerpt={ excerpt }
-                  imageURL={ publicURL }
-                  key={slug}
-                />
-        }) }
-        <div className="container blog-list-main">
-          <div className="arrow">
-            { !isLast && (
-              <Link to={ nextPage } rel="next">
-                <button
-                  type="button"
-                  className="btn btn-default arrow-btn"
-                  onClick={this.handleNext}
-                  >
-                    <FontAwesomeIcon icon={faChevronLeft} className="arrow-icon" />
-                </button> 
-              </Link>
-            )
-            }&nbsp;
-            { !isFirst && (
-              <Link to={ prevPage } rel="prev">
-                <button
-                  type="button"
-                  className="btn btn-default arrow-btn"
-                >
-                  <FontAwesomeIcon icon={faChevronRight} className="arrow-icon" />
-                </button>
-              </Link>
-            ) }
-          </div>
-        </div>
-        <div>
-          <LatestBlog />
-        </div>
-      </Layout>
-     );
-  }
+	render() {
+		const {
+			data: { blog },
+		} = this.props;
+		const posts = this.props.data.allMarkdownRemark.edges;
+		const { totalCount } = this.props.data.allMarkdownRemark;
+		const numPages = Math.ceil(totalCount / 5);
+		const { currentPage } = this.props.pageContext;
+		const isFirst = currentPage === 1;
+		const isLast = currentPage === numPages;
+		typeof window !== `undefined` &&
+			(window.document.title = `${currentPage === 1 ? 'Blog | Ucare' : `Blog | Page ${currentPage} | UCare`}`);
+		const prevPage = currentPage - 1 === 1 ? '/blog' : `/blog/page/${(currentPage - 1).toString()}`;
+		const nextPage = `/blog/page/${(currentPage + 1).toString()}`;
+		return (
+			<Layout>
+				<div
+					className='container-fluid p-0 blog-header text-center'
+					style={{
+						background: `linear-gradient( rgba(50,58,70,.5), rgba(50,58,70,.5) ), url(${blog.publicURL})`,
+						backgroundColor: '#323a46',
+						backgroundPosition: '50%',
+						backgroundSize: 'cover',
+					}}
+				>
+					<div className='centered'>
+						<h1>Blog</h1>
+						<h6>Latest News & Updates from the UCare Team</h6>
+					</div>
+				</div>
+				{posts.map(({ node }) => {
+					const { title, date, featured_image } = node.frontmatter;
+					let publicURL = null;
+					if (featured_image) publicURL = featured_image.publicURL;
+					const { slug } = node.fields;
+					const { excerpt } = node;
+					return (
+						<BlogListTemp title={title} date={date} url={slug} excerpt={excerpt} imageURL={publicURL} key={slug} />
+					);
+				})}
+				<div className='container blog-list-main'>
+					<div className='arrow'>
+						{!isLast && (
+							<Link to={nextPage} rel='next'>
+								<button type='button' className='btn btn-default arrow-btn' onClick={this.handleNext}>
+									<FontAwesomeIcon icon={faChevronLeft} className='arrow-icon' />
+								</button>
+							</Link>
+						)}
+						&nbsp;
+						{!isFirst && (
+							<Link to={prevPage} rel='prev'>
+								<button type='button' className='btn btn-default arrow-btn'>
+									<FontAwesomeIcon icon={faChevronRight} className='arrow-icon' />
+								</button>
+							</Link>
+						)}
+					</div>
+				</div>
+				<div>
+					<LatestBlog />
+				</div>
+			</Layout>
+		);
+	}
 }
 
 export default BlogList;
 export const blogListQuery = graphql`
-  query blogListQuery($skip: Int!, $limit: Int!) {
-    allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC }
-      limit: $limit
-      skip: $skip
-      filter: { frontmatter: { type: {eq: "post"}}}
-    ) {
-      totalCount
-      edges {
-        node {
-          excerpt(pruneLength: 292)
-          fields {
-            slug
-          }
-          frontmatter {
-            url
-            title
-            date(formatString: "DD MMMM, YYYY")
-            featured_image {
-              relativePath
-              publicURL
-              name
-              extension
-            }
-          }
-        }
-      }
-    }
-    blog: file(relativePath: { eq: "page/blog/friends04.jpg" }) {
-      publicURL
-    }
-  }
-`
+	query blogListQuery($skip: Int!, $limit: Int!) {
+		allMarkdownRemark(
+			sort: { fields: [frontmatter___date], order: DESC }
+			limit: $limit
+			skip: $skip
+			filter: { frontmatter: { type: { eq: "post" } } }
+		) {
+			totalCount
+			edges {
+				node {
+					excerpt(pruneLength: 292)
+					fields {
+						slug
+					}
+					frontmatter {
+						url
+						title
+						date(formatString: "DD MMMM, YYYY")
+						featured_image {
+							relativePath
+							publicURL
+							name
+							extension
+						}
+					}
+				}
+			}
+		}
+		blog: file(relativePath: { eq: "page/blog/friends04.jpg" }) {
+			publicURL
+		}
+	}
+`;
